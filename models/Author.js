e82c761b5038e214e2e63d90bddad80fb1cd87b5
poojan14 <pvs09747@gmail.com>
2022-09-28 01:25:21 +0530

    initial author routes

---
+++ models/Author.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const authorSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    }
+})
+module.exports = mongoose.model('Author', authorSchema);
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
+const authorRouter =require('./routes/authors')
+const bodyParser=require('body-parser')
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
@@ -13,7 +16,7 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 //connect db
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -21,7 +24,9 @@ mongoose.connect(process.env.DATABASE_URL, {
 const db= mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', error => console.error('connected to mongoose')) 
+
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3004);
